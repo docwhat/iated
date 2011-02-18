dd18e0f6f5ba95c29f87e22d68141b9ae312f45e
Christian Höltje <docwhat@gerf.org>
2011-02-18 17:41:11 -0500

    Got rid of some un-used imports

--- src/main/java/org/docwhat/iated/rest/Preferences.java
+++ src/main/java/org/docwhat/iated/rest/Preferences.java
@@ -6,7 +6,6 @@ import javax.swing.JFrame;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import org.docwhat.iated.AppState;
 
 import org.docwhat.iated.ui.PreferencesDialog;
 
